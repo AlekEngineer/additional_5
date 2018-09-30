3f2a71c67bf6fd229b694cc5cd237c072cfee8d1
AlekEngineer <calessin@gmail.com>
2018-09-30 19:31:16 +0300

    20 pass

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-    const open = []; //виды открывающих скобок
-    const close = []; //виды закрывающих скобок
+    const open = [];
+    const close = [];
+    const stack = [];
+    let check = 0;
     for (let i=0; i<bracketsConfig.length; i++) { //выделяем откр/закр скобки
-    open.push(bracketsConfig[i][0]);
-    close.push(bracketsConfig[i][1]);
-   };  
-   const str1 = str.split(''); //"разобрали" строку 
-   for (let i = 0; i < open.length; i++) { //переименуем одинаковые скобки 
-    if (open [ i ] == close [ i ]){
-     open [i] = i + "op";
-     close [i] = i + "cl";
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
     };  
- };
-      let stek = [],
-       closeIndex = 0,
-       openIndex = 0;
-
-for (let i = 0; i < str1.length; i++) { //проверим строку посимвольно
-  openIndex = open.indexOf(str1[i]); //открывающие скобки в стек
-  if (openIndex !== -1) {
-      stek.push(openIndex);  
-      continue;
-  }
-
-  closeIndex = close.indexOf(str1[i]); //ищем и чекаем закрывающие скобки открывающей
-  if (closeIndex !== -1) {
-      
-      openIndex = stek.pop();
-      if (closeIndex != openIndex) {
-          return false;
+    const str1 = str.split(''); //"разобрали" строку 
+    for ( let i=0; i<str1.length; i++){ //проверяем строку посимвольно
+      for( let j=0;j<open.length; j++){
+        if(open[j]==close[j] && str1[i]==close[j]){ //частный случай: добрались до открытая==закрытая
+          if(str1[i]==close[stack[stack.length-1]]){ 
+            stack.pop(j);
+            check--;
+          }
+          else{
+            stack.push(j);
+            check++;
+          }
+        }
+        else if(str1[i]==open[j]){ 
+          stack.push(j); //пополнили стек
+          check++;
+        }
+        else if(str1[i]==close[j]){
+          if(j==stack.pop()){ //проверка соответствия закрывающей скобки
+            check--; //закрывающую скобку из стека
+          }
+          else return false;
+        }
       }
-  }
-}
-if (stek.length !== 0) { // чек открытых/закрытых скобок
-   return false;
-  }
-  return true;
+      if(check<0) return false; //чек пар открытых/закрытых скобок
+    }
+    if(check==0) return true;
+    else return false;
 }
